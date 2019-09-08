2dff9367e4289e7f8a6e798522e4d6f402777ccb
Aldwin Vlasblom <aldwin@avaq.it>
2019-09-08 13:05:18 +0200

    Add a feature to allow users to pass files on the CLI

--- cli.mjs
+++ cli.mjs
@@ -4,7 +4,11 @@ import concise from './report-concise.mjs'
 import verbose from './report-verbose.mjs'
 import crawl from './crawler.mjs'
 
-crawl('./test/')
+const eventualFiles = process.argv.length > 2
+  ? Promise.resolve(process.argv.slice(2))
+  : crawl('./test/')
+
+eventualFiles
   .then(files => run(files, process.env.CI ? verbose : concise))
   .then(({ passed, failed }) => failed ? process.exit(1) : process.exit(0))
   .catch(err => {
